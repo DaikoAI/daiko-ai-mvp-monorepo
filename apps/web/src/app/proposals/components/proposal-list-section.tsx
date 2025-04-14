e869cc418d753b427d92320a2b1e31f07833d0dd
Asuma Yamada <yamadaasuma@gmail.com>
2025-04-15 00:25:26 +0900

    fix: unauth status

--- apps/web/src/app/proposals/components/proposal-list-section.tsx
+++ apps/web/src/app/proposals/components/proposal-list-section.tsx
@@ -1,8 +1,15 @@
+import { auth } from "@/server/auth";
 import { api } from "@/trpc/server";
+import { redirect } from "next/navigation";
 import { Suspense } from "react";
 import { ProposalList } from "./proposal-list";
 
 export const ProposalListSection: React.FC = async () => {
+  const session = await auth();
+  if (!session) {
+    return redirect("/onboarding");
+  }
+
   const proposals = await api.proposal.getProposals();
 
   return (
