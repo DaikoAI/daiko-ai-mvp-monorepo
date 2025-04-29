97442e6d2501414c130805718367c16686994753
Asuma Yamada <yamadaasuma@gmail.com>
2025-04-29 22:26:07 +0900

    fix: connection ts

--- packages/shared/src/db/connection.ts
+++ packages/shared/src/db/connection.ts
@@ -1,11 +1,13 @@
+import "dotenv/config";
 import { neon } from "@neondatabase/serverless";
-import dotenv from "dotenv";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 
-dotenv.config();
+if (!process.env.DATABASE_URL) {
+  throw new Error("connection.ts: DATABASE_URL is not set");
+}
 
-const sql = neon(process.env.DATABASE_URL!);
+const sql = neon(process.env.DATABASE_URL);
 // デフォルトのデータベース接続をエクスポート
 export const db = drizzle(sql, { schema });
 export type NeonHttpDatabase = typeof db;
